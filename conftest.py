d2af8416c927b3e59fae4df235ab38fb96d6d6eb
mldtrvs <mentalfvnda@gmail.com>
2023-07-06 23:23:16 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
 from fixture.application import Application
-@pytest.fixture(scope = "session")
+
+
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
