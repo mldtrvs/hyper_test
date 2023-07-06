7fc6a8b73bc64b04623d1345365c7c652f4d1d6b
mldtrvs <mentalfvnda@gmail.com>
2023-07-06 22:19:14 +0200

    выделен класс FilmTypeHelper

--- test/test_addfilmtype_fix.py
+++ test/test_addfilmtype_fix.py
@@ -13,11 +13,11 @@ def test_addfilmtype(app):
 
     app.session.login_HGfilm(username="mentalfvnda@gmail.com", password="retSoHn18")
     #time.sleep(2)
-    wait = app.go_to_film_types_menu()
-    app.add_new_film_type(wait, film_type="eji4t3a4r3")
+    wait = app.filmType.go_to_film_types_menu()
+    app.filmType.add_new(wait, film_type="eji4t3a4r3")
     time.sleep(2)
-    app.search_for_new_film_type()
-    app.check_if_added_delete_check_if_deleted()
+    app.filmType.search_for_new_added()
+    app.filmType.check_if_added_delete_check_if_deleted()
 
 
 
