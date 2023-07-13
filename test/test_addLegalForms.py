838f71820fe070fd2ed1b272e7f53c9cbc3a4d0b
mldtrvs <134957839+mldtrvs@users.noreply.github.com>
2023-07-13 16:20:54 +0200

    Update test_addLegalForms.py

--- test/test_addLegalForms.py
+++ test/test_addLegalForms.py
@@ -9,7 +9,7 @@ def test_addLegalForm(app):
     app.legalForms.search_for_new_added("saDgr785gxc")
     app.legalForms.check_if_added_delete_check_if_deleted(wait)
 
-def test_addLegalForm_SP(app):
+def test_addLegalForm_with_SP_checkbox(app):
     app.menuCategories.go_to_directories()
     wait = app.legalForms.go_to_legal_forms()
     app.legalForms.add_new_SP_attribute_checkbox(wait, "Dgr785gxc")
