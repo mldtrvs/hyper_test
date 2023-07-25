73f5090438be1669667254b6a9c81c85578416db
mldtrvs <mentalfvnda@gmail.com>
2023-07-25 22:09:38 +0200

    trying to chek if tag type is correct

--- fixture/tags.py
+++ fixture/tags.py
@@ -22,8 +22,8 @@ class tagsHelper:
         add_btn.click()
         time.sleep(2)
         self.app.driver.find_element(By.NAME, "tag").send_keys(tag_name)
-        type_selector = self.app.driver.find_element(By.CSS_SELECTOR, "#form-62--1_popup "
-                                                                      ".data-myls__sys_status_type_id .dx-selectbox")
+        type_selector = self.app.driver.find_element(By.CSS_SELECTOR, "#form-62--1_popup .data-myls__sys_tag_type_id "
+                                                                      ".dx-selectbox")
         type_selector.click()
         aria_owns_value = type_selector.get_attribute("aria-owns")
         print(aria_owns_value)
@@ -37,5 +37,62 @@ class tagsHelper:
     def save_form(self, wait):
         time.sleep(4)
         ok_btn = wait.until(
-            EC.visibility_of_element_located((By.ID, "form-16--1_popup_save-button")))
+            EC.visibility_of_element_located((By.ID, "form-62--1_popup_save-button")))
         ok_btn.click()
+    
+    def search_for_new_added(self, tag_name):
+        search_input = self.app.driver.find_element(By.CSS_SELECTOR, "#grid-63_tab [role=textbox]")
+        search_input.click()
+        search_input.clear()
+        search_input.send_keys(tag_name)
+        time.sleep(3)
+
+        idn = self.app.driver.find_element(By.ID, "grid-63_tab_totalId")
+        text = idn.text
+        extracted_number = text.split(":")[1].strip()
+        print(extracted_number)
+        time.sleep(3)
+        wait = WebDriverWait(self.app.driver, 10)
+        typevalue = wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, f"#grid-63_tab [data-id='{extracted_number}']>td:nth-child(3)")))
+        text = typevalue.text
+        print(text)
+
+    def check_total_records(self, expected_count):
+        total_records = self.app.driver.find_element(By.ID, "grid-63_tab_totalCount")
+        total_records_value = total_records.text
+
+        if expected_count in total_records_value:
+            print(f"Total records is {expected_count}.")
+        else:
+            # Assertion failed, handle the failure or raise an exception
+            raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+
+    def delete_record(self, wait):
+        # delete record
+        self.app.driver.find_element(By.CSS_SELECTOR,
+                                     "#grid-63_tab [role=toolbar] [buttonrole=delete]").click()
+        time.sleep(1)
+        delete_current = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-button-type='grid"
+                                                                                 "-63_tab_delete_current']")))
+        delete_current.click()
+        pass
+        time.sleep(3)
+
+    def check_if_added_delete_check_if_deleted(self, wait):
+        # Verify if total records in the grid equals 1
+        total_records = self.app.driver.find_element(By.ID, "grid-63_tab_totalCount")
+        total_records_value = total_records.text
+        expected_count = '1'
+
+        if expected_count in total_records_value:
+            print("Total records is 1. Proceeding to deletion.")
+            time.sleep(1)
+            self.delete_record(wait)
+            time.sleep(3)
+            self.check_total_records(expected_count='0')
+        else:
+            # Assertion failed, handle the failure or raise an exception
+            raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+
+
--- test/test_addTag.py
+++ test/test_addTag.py
@@ -1,10 +1,11 @@
-
+import time
 def test_addtag(app):
     app.menuCategories.go_to_directories()
     wait = app.tags.go_to_tags()
-    for div_index in range(1, 7):
+    for div_index in range(1, 2):
         aria_owns_value = app.tags.open_new_form("zaa1231kjklm", wait)
-        app.tags.choose_status_type(aria_owns_value, div_index)
+        app.tags.choose_tag_type(aria_owns_value, div_index)
         app.tags.save_form(wait)
         app.tags.search_for_new_added("zaa1231kjklm")
+        time.sleep(5)
         app.tags.check_if_added_delete_check_if_deleted(wait)
