4ba435768f24de64aeb0480cf639b24f13b40ddf
mldtrvs <134957839+mldtrvs@users.noreply.github.com>
2023-07-27 16:25:03 +0200

    genres refactored but DELETE icon problems

--- fixture/genres.py
+++ fixture/genres.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,8 +22,7 @@ class GenresHelper:
         add_btn = wait.until(EC.element_to_be_clickable((
             By.CSS_SELECTOR, "#grid-9_tab [role=toolbar] [buttonrole=add]")))
         add_btn.click()
-        time.sleep(2)
-        self.app.driver.find_element(By.NAME, "genre_name_ru").send_keys(genre)
+        self.app.driver.find_element(By.CSS_SELECTOR, "#form-10--1_popup [role=textbox]").send_keys(genre)
         ok_btn = wait.until(
             EC.element_to_be_clickable((By.ID, "form-10--1_popup_save-button")))
         ok_btn.click()
@@ -34,20 +34,25 @@ class GenresHelper:
         search_input.send_keys(genre)
         time.sleep(3)
 
-    def check_total_records(self, expected_count):
+    def check_if_added(self):
         total_records = self.app.driver.find_element(By.ID, "grid-9_tab_totalCount")
         total_records_value = total_records.text
-
-        if expected_count in total_records_value:
-            print(f"Total records is {expected_count}.")
-        else:
-            # Assertion failed, handle the failure or raise an exception
-            raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+        expected_count = '1'
+        try:
+            if expected_count in total_records_value:
+                print("Total records is 1. Proceeding to deletion.")
+            else:
+                # Assertion failed, handle the failure or raise an exception
+                raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+        except AssertionError as e:
+            pytest.fail(f"Test failed: {e}")
 
     def delete_record(self, wait):
-        # delete record
-        self.app.driver.find_element(By.CSS_SELECTOR,
-                                     "#grid-9_tab [role=toolbar] [buttonrole=delete]").click()
+        delete_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                            "#grid-9_tab [role=toolbar] [buttonrole=delete]")))
+        delete_btn.click()
+        # self.app.driver.find_element(By.CSS_SELECTOR,
+        #                              "#grid-9_tab [role=toolbar] [buttonrole=delete]").click()
         time.sleep(1)
         delete_current = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-button-type='grid"
                                                                                  "-9_tab_delete_current']")))
@@ -55,18 +60,13 @@ class GenresHelper:
         pass
         time.sleep(3)
 
-    def check_if_added_delete_check_if_deleted(self, wait):
-        # Verify if total records in the grid equals 1
+    def check_if_deleted(self):
         total_records = self.app.driver.find_element(By.ID, "grid-9_tab_totalCount")
         total_records_value = total_records.text
-        expected_count = '1'
+        expected_count = '0'
 
         if expected_count in total_records_value:
-            print("Total records is 1. Proceeding to deletion.")
-            time.sleep(1)
-            self.delete_record(wait)
-            time.sleep(3)
-            self.check_total_records(expected_count='0')
+            print("Total records is 0.")
         else:
             # Assertion failed, handle the failure or raise an exception
             raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
--- test/test_addGenre.py
+++ test/test_addGenre.py
@@ -7,4 +7,6 @@ def test_addGenre(app):
     app.genres.add_new(wait, "pyuic5456dsc")
     time.sleep(2)
     app.genres.search_for_new_added("pyuic5456dsc")
-    app.genres.check_if_added_delete_check_if_deleted(wait)
+    app.genres.check_if_added()
+    app.genres.delete_record(wait)
+    app.genres.check_if_deleted()
