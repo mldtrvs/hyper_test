ed1355c6a92375548b0078c5bac674ddbc1fbfb9
mldtrvs <mentalfvnda@gmail.com>
2023-07-27 22:23:54 +0200

    Update test_addLegalForms.py

--- test/test_addLegalForms.py
+++ test/test_addLegalForms.py
@@ -12,13 +12,13 @@ def test_addLegalForm(app):
     app.legalForms.delete_record(wait)
     app.legalForms.check_if_deleted()
 
-# def test_addLegalForm_with_SP_checkbox(app):
-#     app.menuCategories.go_to_directories()
-#     app.legalForms.go_to_legal_forms()
-#     wait = app.legalForms.add_new_SP_attribute_checkbox("Dgr785gxc")
-#     time.sleep(2)
-#     app.legalForms.search_for_new_added("Dgr785gxc")
-#     app.legalForms.get_checkbox_value()
-#     app.legalForms.check_if_added()
-#     app.legalForms.delete_record(wait)
-#     app.legalForms.check_if_deleted()
+def test_addLegalForm_with_SP_checkbox(app):
+    app.menuCategories.go_to_directories()
+    app.legalForms.go_to_legal_forms()
+    wait = app.legalForms.add_new_SP_attribute_checkbox("Dgr785gxc")
+    time.sleep(2)
+    app.legalForms.search_for_new_added("Dgr785gxc")
+    app.legalForms.get_checkbox_value()
+    app.legalForms.check_if_added()
+    app.legalForms.delete_record(wait)
+    app.legalForms.check_if_deleted()
