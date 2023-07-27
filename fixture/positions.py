a370714a9d1c2aaea2298cee4244c8fc9704eecf
mldtrvs <mentalfvnda@gmail.com>
2023-07-27 18:45:01 +0200

    positions refactored

--- fixture/positions.py
+++ fixture/positions.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium.common import TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -20,7 +21,7 @@ class positionsHelper:
             self.scroll_menu(wait)
             positions = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".panel-list a[href='#grid-383_tab']")))
         positions.click()
-        return positions
+        return wait
 
     def scroll_menu(self, wait):
         menu_scrollbar = wait.until(
@@ -29,13 +30,11 @@ class positionsHelper:
         action_chains.move_to_element(menu_scrollbar).perform()
         action_chains.drag_and_drop_by_offset(menu_scrollbar, 0, 164).perform()
 
-    def add_new(self, position_name):
-        wait = WebDriverWait(self.app.driver, 10)
+    def add_new(self, wait, position_name):
         add_btn = wait.until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, '#grid-383_tab [role=toolbar] [buttonrole=add]')))
         add_btn.click()
-        time.sleep(2)
-        self.app.driver.find_element(By.NAME, "post_name").send_keys(position_name)
+        self.app.driver.find_element(By.CSS_SELECTOR, "#form-457--1_popup [role=textbox]").send_keys(position_name)
         ok_btn = wait.until(EC.element_to_be_clickable((By.ID, 'form-457--1_popup_save-button')))
         ok_btn.click()
 
@@ -46,40 +45,37 @@ class positionsHelper:
         search_input.send_keys(status_name)
         time.sleep(3)
 
-    def check_total_records(self, expected_count):
+    def check_if_added(self):
         total_records = self.app.driver.find_element(By.ID, "grid-383_tab_totalCount")
         total_records_value = total_records.text
+        expected_count = '1'
+        try:
+            if expected_count in total_records_value:
+                print("Total records is 1. Proceeding to deletion.")
+            else:
+                # Assertion failed, handle the failure or raise an exception
+                raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+        except AssertionError as e:
+            pytest.fail(f"Test failed: {e}")
 
-        if expected_count in total_records_value:
-            print(f"Total records is {expected_count}.")
-        else:
-            # Assertion failed, handle the failure or raise an exception
-            raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
+    def delete_record(self, wait):
 
-    def delete_record(self):
-        # delete record
         self.app.driver.find_element(By.CSS_SELECTOR,
                                      "#grid-383_tab [role=toolbar] [buttonrole=delete]").click()
         time.sleep(1)
-        wait = WebDriverWait(self.app.driver, 10)
         delete_current = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-button-type='grid"
                                                                                  "-383_tab_delete_current']")))
         delete_current.click()
         pass
         time.sleep(3)
 
-    def check_if_added_delete_check_if_deleted(self):
-        # Verify if total records in the grid equals 1
+    def check_if_deleted(self):
         total_records = self.app.driver.find_element(By.ID, "grid-383_tab_totalCount")
         total_records_value = total_records.text
-        expected_count = '1'
+        expected_count = '0'
 
         if expected_count in total_records_value:
-            print("Total records is 1. Proceeding to deletion.")
-            time.sleep(1)
-            self.delete_record()
-            time.sleep(3)
-            self.check_total_records(expected_count='0')
+            print("Total records is 0.")
         else:
             # Assertion failed, handle the failure or raise an exception
             raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
--- test/test_addPositions.py
+++ test/test_addPositions.py
@@ -4,7 +4,9 @@ import time
 def test_addPosition(app):
     app.menuCategories.go_to_directories()
     wait = app.positions.go_to_positions()
-    app.positions.add_new("hijnoii257")
+    app.positions.add_new(wait, "hijnoii257")
     time.sleep(2)
     app.positions.search_for_new_added("hijnoii257")
-    app.positions.check_if_added_delete_check_if_deleted(wait)
+    app.positions.check_if_added()
+    app.positions.delete_record(wait)
+    app.positions.check_if_deleted()
