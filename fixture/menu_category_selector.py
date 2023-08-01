70e56f17d977eea37bbe1eef50699d812458259e
mldtrvs <mentalfvnda@gmail.com>
2023-08-01 22:33:48 +0200

    Update menu_category_selector.py

--- fixture/menu_category_selector.py
+++ fixture/menu_category_selector.py
@@ -15,7 +15,8 @@ class menuCategorySelectorHelper:
                                                             "8)>.dx-list-group-body")
         if not directories_dropdown.is_displayed():
             wait = WebDriverWait(self.app.driver, 20)
-            directories = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")))
+            directories = wait.until(EC.element_to_be_clickable((
+                By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")))
             # directories = self.app.driver.find_element(
             #     By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")
             directories.click()
