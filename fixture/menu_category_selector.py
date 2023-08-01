ef8143b138d98959d6357e167be82affa405c946
mldtrvs <mentalfvnda@gmail.com>
2023-08-01 22:33:05 +0200

    category selector WAIT added

--- fixture/menu_category_selector.py
+++ fixture/menu_category_selector.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class menuCategorySelectorHelper:
 
@@ -11,6 +14,9 @@ class menuCategorySelectorHelper:
                                                             ".panel-list [role=listbox]>div:nth-child("
                                                             "8)>.dx-list-group-body")
         if not directories_dropdown.is_displayed():
-            directories = self.app.driver.find_element(
-                By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")
+            wait = WebDriverWait(self.app.driver, 20)
+            directories = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")))
+            # directories = self.app.driver.find_element(
+            #     By.CSS_SELECTOR, ".panel-list [role=listbox]>div:nth-child(8)>div:first-child")
             directories.click()
+            return wait
