f1f753ee8f26730ba786419f96488474d2179c77
mldtrvs <mentalfvnda@gmail.com>
2023-08-10 22:00:55 +0200

    genres EDIT added

--- fixture/genres.py
+++ fixture/genres.py
@@ -44,24 +44,48 @@ class GenresHelper:
         expected_count = '1'
         try:
             if expected_count in total_records_value:
-                print("Total records is 1. Proceeding to deletion.")
+                print("Total records is 1. Proceeding to next step.")
             else:
                 # Assertion failed, handle the failure or raise an exception
                 raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
         except AssertionError as e:
             pytest.fail(f"Test failed: {e}")
 
+    def edit(self, wait, edit_ru, edit_en):
+        record_id_number = self.get_record_id()
+
+        edit_btn = wait.until(EC.element_to_be_clickable((
+            By.CSS_SELECTOR, "#grid-9_tab [role=toolbar] [buttonrole=edit]")))
+        edit_btn.click()
+        genre_ru_edit = self.app.driver.find_element(
+            By.CSS_SELECTOR, f"#form-10-{record_id_number}_popup [name='genre_name_ru']")
+        genre_ru_edit.clear()
+        genre_ru_edit.send_keys(edit_ru)
+
+        genre_en_edit = self.app.driver.find_element(By.CSS_SELECTOR,
+                                                     f"#form-10-{record_id_number}_popup [name='genre_name_en']")
+        genre_en_edit.clear()
+        genre_en_edit.send_keys(edit_en)
+
+        ok_btn = wait.until(
+            EC.element_to_be_clickable((By.ID, f"form-10-{record_id_number}_popup_save-button")))
+        ok_btn.click()
+
+    def get_record_id(self):
+        record_id = self.app.driver.find_element(By.ID, "grid-9_tab_totalId")
+        text = record_id.text
+        record_id_number = text.split(":")[1].strip()
+        # print(record_id_number)
+        return record_id_number
+
     def delete_record(self, wait):
         delete_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                             "#grid-9_tab [role=toolbar] [buttonrole=delete]")))
         delete_btn.click()
-        # self.app.driver.find_element(By.CSS_SELECTOR,
-        #                              "#grid-9_tab [role=toolbar] [buttonrole=delete]").click()
         time.sleep(1)
         delete_current = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-button-type='grid"
                                                                                  "-9_tab_delete_current']")))
         delete_current.click()
-        pass
         time.sleep(3)
 
     def check_if_deleted(self):
--- test/test_addGenre.py
+++ test/test_addGenre.py
@@ -4,11 +4,20 @@ import time
 def test_addGenre(app):
     app.menuCategories.go_to_directories()
     wait = app.genres.go_to_genres()
+
     app.genres.add_new(wait, "pyuic5456dsc", "tyutbg12fed")
     time.sleep(2)
     app.genres.search_for_new_added("pyuic5456dsc")
     app.genres.check_if_added()
     app.genres.search_for_new_added("tyutbg12fed")
     app.genres.check_if_added()
+
+    app.genres.edit(wait, "yrhgd34das", "qwexcsxc213")
+    time.sleep(2)
+    app.genres.search_for_new_added("yrhgd34das")
+    app.genres.check_if_added()
+    app.genres.search_for_new_added("qwexcsxc213")
+    app.genres.check_if_added()
+
     app.genres.delete_record(wait)
     app.genres.check_if_deleted()
