a97b740740a17729e2b3c1b894459ca7a124ec4e
mldtrvs <134957839+mldtrvs@users.noreply.github.com>
2023-08-11 10:41:15 +0200

    adress type EDIT added

--- fixture/addressType.py
+++ fixture/addressType.py
@@ -22,7 +22,7 @@ class addressTypeHelper:
         add_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#grid-310_tab [role=toolbar] ["
                                                                           "buttonrole=add]")))
         add_btn.click()
-        self.app.driver.find_element(By.CSS_SELECTOR, "#form-311--1_popup [role=textbox]").send_keys(address_type)
+        self.app.driver.find_element(By.CSS_SELECTOR, "#form-311--1_popup [name='type_name']").send_keys(address_type)
         ok_btn = wait.until(EC.element_to_be_clickable((By.ID, 'form-311--1_popup_save-button')))
         ok_btn.click()
 
@@ -39,12 +39,34 @@ class addressTypeHelper:
         expected_count = '1'
         try:
             if expected_count in total_records_value:
-                print("Total records is 1. Proceeding to deletion.")
+                print("Total records is 1. Proceeding to next step.")
             else:
                 # Assertion failed, handle the failure or raise an exception
                 raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
         except AssertionError as e:
             pytest.fail(f"Test failed: {e}")
+
+    def edit(self, wait, address_type):
+        record_id_number = self.get_record_id()
+
+        edit_btn = wait.until(EC.element_to_be_clickable((
+            By.CSS_SELECTOR, "#grid-310_tab [role=toolbar] [buttonrole=edit]")))
+        edit_btn.click()
+        address_type_edit = self.app.driver.find_element(
+            By.CSS_SELECTOR, f"#form-311-{record_id_number}_popup [name='type_name']")
+        address_type_edit.clear()
+        address_type_edit.send_keys(address_type)
+
+        ok_btn = wait.until(
+            EC.element_to_be_clickable((By.ID, f"form-311-{record_id_number}_popup_save-button")))
+        ok_btn.click()
+
+    def get_record_id(self):
+        record_id = self.app.driver.find_element(By.ID, "grid-310_tab_totalId")
+        text = record_id.text
+        record_id_number = text.split(":")[1].strip()
+        # print(record_id_number)
+        return record_id_number
 
     def delete_record(self, wait):
 
--- test/test_addAddressType.py
+++ test/test_addAddressType.py
@@ -4,9 +4,16 @@ import time
 def test_addAddressType(app):
     app.menuCategories.go_to_directories()
     wait = app.addressType.go_to_address_types()
+
     app.addressType.add_new(wait, "kamxc123masd")
     time.sleep(2)
     app.addressType.search_for_new_added("kamxc123masd")
     app.addressType.check_if_added()
+
+    app.addressType.edit(wait, "sdfe213dsfd")
+    time.sleep(2)
+    app.addressType.search_for_new_added("sdfe213dsfd")
+    app.addressType.check_if_added()
+
     app.addressType.delete_record(wait)
     app.addressType.check_if_deleted()
