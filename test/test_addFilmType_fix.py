6c0f05135978b8f4157c75b782515899a3651889
mldtrvs <134957839+mldtrvs@users.noreply.github.com>
2023-08-11 10:50:20 +0200

    film type EDIT added

--- test/test_addFilmType_fix.py
+++ test/test_addFilmType_fix.py
@@ -4,10 +4,17 @@ import time
 def test_addFilmType(app):
     app.menuCategories.go_to_directories()
     wait = app.filmType.go_to_film_types_menu()
+
     app.filmType.add_new(wait, "eji4t3a4r3")
     time.sleep(2)
     app.filmType.search_for_new_added("eji4t3a4r3")
     app.filmType.check_if_added()
+
+    app.filmType.edit(wait, "mmjag341rds")
+    time.sleep(2)
+    app.filmType.search_for_new_added("mmjag341rds")
+    app.filmType.check_if_added()
+
     app.filmType.delete_record(wait)
     app.filmType.check_if_deleted()
 
