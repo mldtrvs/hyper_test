19248786bedb490393c5471bc930fadf6afab612
mldtrvs <mentalfvnda@gmail.com>
2023-09-19 22:12:43 +0200

    с проверками будет весело

--- fixture/projects.py
+++ fixture/projects.py
@@ -94,22 +94,18 @@ class projectHelper:
         checkboxes = wait.until(
             EC.presence_of_all_elements_located((
                 By.CSS_SELECTOR, f"#{gt_aria_owns_value} [role=option]")))
-        total_checkboxes = len(checkboxes)
-        print(total_checkboxes)
+
         checkboxes_text = []
         for checkbox in checkboxes:
             # Using JavaScript to retrieve the text of the checkbox, even if it's not visible
             checkbox_text = self.app.driver.execute_script("return arguments[0].textContent;", checkbox)
             checkboxes_text.append(checkbox_text)
 
-            # Print the text of all checkboxes
-        for checkbox_text in checkboxes_text:
-            print(checkbox_text)
-
         # Generate a random number of checkboxes to select
         min_checkboxes_to_select = 1  # Minimum number of checkboxes to select
-        max_checkboxes_to_select = len(checkboxes_text)  # Maximum number of checkboxes to select
-        num_checkboxes_to_select = random.randint(min_checkboxes_to_select, max_checkboxes_to_select)
+        # max_checkboxes_to_select = len(checkboxes_text)  # Maximum number of checkboxes to select
+        # num_checkboxes_to_select = random.randint(min_checkboxes_to_select, max_checkboxes_to_select)
+        num_checkboxes_to_select = random.randint(1, 6)
 
         # Randomly select checkboxes
         random_checkboxes = random.sample(checkboxes_text, num_checkboxes_to_select)
@@ -131,9 +127,10 @@ class projectHelper:
             if i < len(random_checkboxes) - 1:
                 genre_type_input.send_keys(Keys.ENTER)
 
+        time.sleep(1)
         self.app.driver.find_element(By.CSS_SELECTOR, "#form-277--1_popup-tab_0_image_file-image").click()
 
-        return gt_aria_owns_value
+        return gt_aria_owns_value, random_checkboxes
 
     def save_form(self, wait):
         ok_btn = wait.until(
@@ -148,6 +145,54 @@ class projectHelper:
         search_input.clear()
         search_input.send_keys(title)
 
+    def get_record_id(self):
+        record_id = self.app.driver.find_element(By.ID, "grid-276_tab_totalId")
+        text = record_id.text
+        record_id_number = text.split(":")[1].strip()
+        # print(record_id_number)
+        return record_id_number
+
+    def scroll_horizontally_until_visible(self, element):
+        current_scroll_left = 0
+        max_scroll_left = self.app.driver.execute_script(
+            "return document.documentElement.scrollWidth - window.innerWidth;")
+
+        while current_scroll_left < max_scroll_left:
+            try:
+                # Try to find the element
+                element.is_displayed()
+                break  # Element is visible, exit the loop
+            except:
+                # Element is not visible, scroll right
+                current_scroll_left += 100  # Adjust the scroll amount as needed
+                self.app.driver.execute_script(f"window.scrollTo({current_scroll_left}, 0);")
+                time.sleep(1)  # Add a sleep to allow the page to load and update
+
+    def get_production_type_value(self):
+        record_id_number = self.get_record_id()
+        print(record_id_number)
+
+        wait = WebDriverWait(self.app.driver, 10)
+        production_type_selector = (By.CSS_SELECTOR, f"#grid-276_tab [data-id='{record_id_number}']")
+        production_type_value = wait.until(
+            EC.presence_of_element_located(production_type_selector))
+
+        self.scroll_horizontally_until_visible(production_type_value)
+
+        # production_type_value = wait.until(
+        #     EC.visibility_of_element_located((By.CSS_SELECTOR, f"#grid-276_tab [data-id='{record_id_number}']")))
+
+        production_type_text_out = production_type_value.text
+        print(production_type_text_out)
+        return production_type_text_out
+
+    def check_selected_production_type_match(self, production_type_text_in, production_type_text_out):
+        if production_type_text_in in production_type_text_out:
+            print("Tag type match")
+        else:
+            raise AssertionError(
+                f"Tag type values differ. Expected: {production_type_text_in}, Actual: {production_type_text_out}")
+
     def check_if_added(self):
         total_records = self.app.driver.find_element(By.ID, "grid-276_tab_totalCount")
         total_records_value = total_records.text
@@ -160,3 +205,25 @@ class projectHelper:
                 raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
         except AssertionError as e:
             pytest.fail(f"Test failed: {e}")
+
+    def delete_record(self, wait):
+
+        self.app.driver.find_element(By.CSS_SELECTOR,
+                                     "#grid-276_tab [role=toolbar] [buttonrole=delete]").click()
+        time.sleep(1)
+        delete_current = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-button-type='grid"
+                                                                                 "-276_tab_delete_current']")))
+        delete_current.click()
+        pass
+        time.sleep(4)
+
+    def check_if_deleted(self):
+        total_records = self.app.driver.find_element(By.ID, "grid-276_tab_totalCount")
+        total_records_value = total_records.text
+        expected_count = '0'
+
+        if expected_count in total_records_value:
+            print("Total records is 0.")
+        else:
+            # Assertion failed, handle the failure or raise an exception
+            raise AssertionError(f"Expected {expected_count} record, but found {total_records_value} records.")
--- test/test_ProjectGenre.py
+++ test/test_ProjectGenre.py
@@ -5,12 +5,17 @@ def test_project_genre(app):
     app.menuCategories.go_to_general()
     wait = app.projects.go_to_projects()
 
-    aria_owns_value = app.projects.open_new_form(wait, "werwe213adsc")
+    aria_owns_value = app.projects.open_new_form(wait, "pewqew123asd")
     # app.projects.production_type_own_production(aria_owns_value, wait)
     production_type_text_in = app.projects.random_choice_production_type(aria_owns_value)
-    # div_index_gt = 0  # Define the index you want to exclude
-    gt_aria_owns_value = app.projects.random_choice_genre_type(wait)
+    app.projects.random_choice_genre_type(wait)
     app.projects.save_form(wait)
-    # app.projects.search_for_new_added(wait, "werwe213adsc")
-    # time.sleep(4)
-    # app.projects.check_if_added()
+
+    app.projects.search_for_new_added(wait, "pewqew123asd")
+    time.sleep(4)
+    production_type_text_out = app.projects.get_production_type_value()
+    app.projects.check_selected_production_type_match(production_type_text_in, production_type_text_out)
+    app.projects.check_if_added()
+
+    app.projects.delete_record(wait)
+    app.projects.check_if_deleted()
