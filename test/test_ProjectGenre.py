705020d10515e517fdcd4dc614736b021a4364ca
mldtrvs <mentalfvnda@gmail.com>
2023-10-26 22:28:02 +0200

    вроде работает проверка жанра, но надо проверить проверку

--- test/test_ProjectGenre.py
+++ test/test_ProjectGenre.py
@@ -8,7 +8,7 @@ def test_project_genre(app):
     aria_owns_value = app.projects.open_new_form(wait, "pewqew123asd")
     # app.projects.production_type_own_production(aria_owns_value, wait)
     production_type_text_in = app.projects.random_choice_production_type(aria_owns_value)
-    random_genre = app.projects.random_choice_genre_type(wait)
+    selected_genre = app.projects.random_choice_genre_type(wait)
     app.projects.save_form(wait)
 
     app.projects.search_for_new_added(wait, "pewqew123asd")
@@ -16,7 +16,7 @@ def test_project_genre(app):
     production_type_text_out = app.projects.get_production_type_td_value()
     app.projects.check_selected_production_type_match(production_type_text_in, production_type_text_out)
     project_tr_text_out = app.projects.get_genre_value(wait)
-    app.projects.check_selected_genres_match(random_genre, project_tr_text_out)
+    app.projects.check_selected_genres_match(selected_genre, project_tr_text_out)
     app.projects.check_if_added()
 
     app.projects.delete_record(wait)
